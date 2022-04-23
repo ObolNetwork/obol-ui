8dfdb97486b03233344ef788805835b56ac23919
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-04-23 20:06:34 +0200

    adds components on index export atoms

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,4 +1,8 @@
+export { SvgIcon } from "./SvgIcon/SvgIcon";
 export { Box } from "./Box/Box";
 export { Button } from "./Button/Button";
 export { Link } from "./Link/Link";
+export { Text } from "./Text/Text";
+
 export * as Tabs from "./Tabs/Tabs";
+export * from "./icons/index";
