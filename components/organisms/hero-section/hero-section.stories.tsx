c7ff87f3ab2227fd6556a607d232c6304625290e
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-05-20 13:37:09 -0600

    adds hero section as organisms

---
+++ components/organisms/hero-section/hero-section.stories.tsx
@@ -0,0 +1,53 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button, Text } from "../../atoms";
+import { HeroSection, HeroSectionProps } from "./hero-section";
+
+export default {
+  title: "Design System/Organisms/HeroSection",
+  component: HeroSection,
+} as ComponentMeta<typeof HeroSection>;
+
+const Template: ComponentStory<typeof HeroSection> = (
+  args: HeroSectionProps
+) => <HeroSection {...args}></HeroSection>;
+
+export const Default = Template.bind({});
+
+const DescriptionContent = () => (
+  <>
+    The Obol Network is an ecosystem for trust minimized staking that allows
+    people to{" "}
+    <Text css={{ display: "inline-block" }} color="create" glow>
+      create
+    </Text>
+    ,
+    <Text css={{ display: "inline" }} color="test" glow>
+      {" "}
+      test
+    </Text>
+    ,
+    <Text css={{ display: "inline" }} color="obolGreen" glow>
+      {" "}
+      run
+    </Text>{" "}
+    &
+    <Text css={{ display: "inline" }} color="coordinate" glow>
+      {" "}
+      co-ordinate
+    </Text>{" "}
+    distributed validators
+  </>
+);
+Default.args = {
+  heading: "Building Distributed Validators for Ethereum",
+  content: <DescriptionContent />,
+  children: <Button>Read the Docs</Button>,
+};
+
+export const DvLaunchpad = Template.bind({});
+
+DvLaunchpad.args = {
+  heading: "Distribute Your Validators",
+  content:
+    "Create an Obol Cluster solo or with a group using the Distributed Validator Launchpad.",
+};
---
+++ components/organisms/hero-section/hero-section.tsx
@@ -0,0 +1,142 @@
+import { Box, Text } from "../../atoms";
+import { mediaQueryKeys, CSS } from "../../../stitches.config";
+import Image from "next/image";
+import { useMediaQuery } from "../../utils/hooks";
+
+export interface HeroSectionProps {
+  heading: string;
+  content: JSX.Element | string;
+  children?: React.ReactNode;
+  image?: { basePath: string; mobilePath?: string };
+  css?: CSS;
+}
+
+const BASE_PATH = "/assets/hexapod.svg";
+const MOBILE_PATH = "/assets/hexapod-mobile.svg";
+
+export const HeroSection: React.FC<HeroSectionProps> = ({
+  image,
+  ...props
+}): JSX.Element => {
+  const screenDownSm = useMediaQuery(mediaQueryKeys.sm);
+  return (
+    <Box
+      css={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        "@sm": { flexDirection: "column" },
+        width: "$full",
+        ...props?.css,
+      }}
+    >
+      <Box
+        css={{
+          display: "flex",
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          "@sm": { alignItems: "initial" },
+          gap: "$xl",
+        }}
+      >
+        <Box
+          css={{
+            display: "flex",
+            gap: "1rem",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+
+            "@sm": {
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: "$2xl",
+            },
+          }}
+        >
+          <Text
+            id="heading-text"
+            css={{
+              fontWeight: "$bold",
+              lineHeight: "calc($xl * 2)",
+              fontSize: "calc($10 + $3)",
+              "@sm": {
+                textAlign: "center",
+                fontSize: "$10",
+              },
+            }}
+            color="textLight"
+          >
+            {props.heading}
+          </Text>
+          {screenDownSm && (
+            <Box
+              css={{
+                flex: 1,
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                "@sm": {
+                  width: "350px",
+                },
+              }}
+            >
+              {!screenDownSm ? (
+                <Image
+                  src={image?.basePath || BASE_PATH}
+                  alt="Obol Logo"
+                  width={912}
+                  height={597}
+                />
+              ) : (
+                <Image
+                  src={image?.basePath || MOBILE_PATH}
+                  alt="Obol Logo"
+                  width={343}
+                  height={226}
+                />
+              )}
+            </Box>
+          )}
+          <Text
+            css={{
+              width: "80%",
+              lineHeight: "$xl",
+              "@sm": {
+                textAlign: "center",
+                width: "auto",
+              },
+            }}
+            size={{ "@initial": "7", "@sm": "5" }}
+            color="textMiddle"
+          >
+            {props.content}
+          </Text>
+        </Box>
+
+        {props.children}
+      </Box>
+      {!screenDownSm && (
+        <Box
+          css={{
+            flex: 1,
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src={image?.basePath || MOBILE_PATH}
+            alt="Obol Logo"
+            width={912}
+            height={597}
+          />
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default HeroSection;
