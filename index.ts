08507ffaa5313d0b48904f96379e5fdcb586218f
eDAX <filanderuclez@gmail.com>
2022-05-23 14:22:31 -0600

    adds missing export of components (#32)

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 export * from "./components/atoms";
+export * from "./components/icons";
+export * from "./components/molecules";
+export * from "./components/organisms";
+export * from "./components/utils/hooks";
 
 export {
-    styled,
-    css,
-    theme,
-    createTheme,
-    getCssText,
-    globalCss,
-    keyframes,
-    config,
-  } from './stitches.config';
+  styled,
+  css,
+  theme,
+  createTheme,
+  getCssText,
+  globalCss,
+  keyframes,
+  config,
+} from "./stitches.config";
