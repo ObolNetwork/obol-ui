16d1eef014e48f4943500974f354f72132d9d980
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-07-03 21:25:52 -0600

    adds automatic year

--- components/organisms/Footer/Footer.tsx
+++ components/organisms/Footer/Footer.tsx
@@ -10,9 +10,15 @@ const defaultLinks: FooterLinksProps[] = [
   {
     title: "Docs",
     links: [
-      { title: "Intro to Obol", link: "https://docs.obol.tech/docs/int/Overview" },
+      {
+        title: "Intro to Obol",
+        link: "https://docs.obol.tech/docs/int/Overview",
+      },
       { title: "Charon", link: "https://github.com/ObolNetwork/charon" },
-      { title: "DV Launchpad", link: "https://docs.obol.tech/docs/dvk/distributed_validator_launchpad" },
+      {
+        title: "DV Launchpad",
+        link: "https://docs.obol.tech/docs/dvk/distributed_validator_launchpad",
+      },
     ],
   },
   {
@@ -33,62 +39,65 @@ const defaultLinks: FooterLinksProps[] = [
 
 export const Footer: React.FC<{ links?: FooterLinksProps[] }> = ({
   links = defaultLinks,
-}) => (
-  <Box
-    css={{
-      display: "flex",
-      "@sm": {
-        flexDirection: "column",
-        gap: "$xl",
-      },
-    }}
-  >
+}) => {
+  const year = new Date().getFullYear();
+  return (
     <Box
       css={{
         display: "flex",
-        flexDirection: "column",
-        gap: "$xxs",
-        flex: 1,
-      }}
-    >
-      <ObolSolidDarkBgH />
-      <Text variant="body" css={{ fontSize: "$3" }}>
-        Copyright © 2022 Obol Labs, Inc.
-      </Text>
-    </Box>
-    <Box
-      className="footer-obol"
-      css={{
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
         "@sm": {
-          gridTemplateColumns: "repeat(2, 1fr)",
+          flexDirection: "column",
           gap: "$xl",
         },
-        flex: 2,
       }}
     >
-      {links.map((item, index) => (
-        <Box
-          key={`link-section-${index}`}
-          css={{ display: "flex", flexDirection: "column", gap: "10px" }}
-        >
-          <Text variant="body" css={{ fontSize: "$5", fontWeight: "$bold" }}>
-            {item.title}
-          </Text>
-          {item.links.map((link, idx) => (
-            <Link
-              key={`link-${idx}`}
-              variant="docs"
-              target="_blank"
-              href={link.link}
-              css={{ color: "$textLight", fontSize: "$4" }}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </Box>
-      ))}
+      <Box
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "$xxs",
+          flex: 1,
+        }}
+      >
+        <ObolSolidDarkBgH />
+        <Text variant="body" css={{ fontSize: "$3" }}>
+          {`Copyright © ${year} Obol Labs, Inc.`}
+        </Text>
+      </Box>
+      <Box
+        className="footer-obol"
+        css={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          "@sm": {
+            gridTemplateColumns: "repeat(2, 1fr)",
+            gap: "$xl",
+          },
+          flex: 2,
+        }}
+      >
+        {links.map((item, index) => (
+          <Box
+            key={`link-section-${index}`}
+            css={{ display: "flex", flexDirection: "column", gap: "10px" }}
+          >
+            <Text variant="body" css={{ fontSize: "$5", fontWeight: "$bold" }}>
+              {item.title}
+            </Text>
+            {item.links.map((link, idx) => (
+              <Link
+                key={`link-${idx}`}
+                variant="docs"
+                target="_blank"
+                href={link.link}
+                css={{ color: "$textLight", fontSize: "$4" }}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </Box>
+        ))}
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
