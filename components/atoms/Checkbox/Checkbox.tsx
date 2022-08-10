f61dfa7916dae6079eaf13bb42e7f29587772ff3
HananINouman <110400366+HananINouman@users.noreply.github.com>
2022-08-10 21:48:07 +0300

    add checkbox atom (#75)
    * add checkboxCard
    
    * checkbox atom edits
    
    * add rotate varient
    
    * fix radius val
    
    Co-authored-by: HananIbrahimNouman <hanan.nouman@tuwaiq.edu.sa>

---
+++ components/atoms/Checkbox/Checkbox.tsx
@@ -0,0 +1,28 @@
+import { styled } from "../../../stitches.config";
+import { CheckboxIcon } from "../../icons";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import React from "react";
+
+export const CheckboxRoot = styled(CheckboxPrimitive.Root, {
+  bc: "$bg03",
+  size: "$lg",
+  border: "2px solid $body",
+  br: "$1",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+export const CheckboxIndicator = CheckboxPrimitive.Indicator;
+
+interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
+
+export const Checkbox: React.FC<CheckboxProps> = ({ ...props }) => {
+  return (
+    <CheckboxRoot {...props}>
+      <CheckboxIndicator>
+        <CheckboxIcon />
+      </CheckboxIndicator>
+    </CheckboxRoot>
+  );
+};
