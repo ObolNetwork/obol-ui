9040008381d90cc3b1990e055c4adcbe9671ef47
HananIbrahimNouman <hanan.nouman@tuwaiq.edu.sa>
2022-10-24 15:34:42 +0300

    element as heading example

--- components/molecules/NotificationCard/NotificationCard.stories.tsx
+++ components/molecules/NotificationCard/NotificationCard.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { NotificationCard } from "./NotificationCard";
+import { Text } from "../../atoms";
 
 export default {
   title: "Design System/Molecules/NotificationCard",
@@ -29,4 +30,30 @@ WithSubheadingAndLink.args = {
   heading: "You will need to set the terms of the proposed cluster to create",
   subHeading: `An ENR is an "Ethereum Node Record", and is used to identify your client to the other clients in the cluster.`,
   link: "https://docs.obol.tech",
+};
+
+export const WithElmentsAsHeading = Template.bind({});
+
+WithElmentsAsHeading.args = {
+  heading: (
+    <>
+      Do not submit any transaction with a{" "}
+      <Text
+        css={{
+          border: " 1px solid $create",
+          background: "rgba(60, 210, 221, 0.1)",
+          display: "inline-block",
+          borderRadius: "$1",
+          padding: "0 $xxxs",
+          color: "$create",
+          fontSize: "$3",
+          lineHeight: "16px",
+        }}
+      >
+        deposit_data
+      </Text>{" "}
+      file that you did not create yourself, or that you do not own the mnemonic
+      phrase for.
+    </>
+  ),
 };
