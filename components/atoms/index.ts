2635a945231b128afdacc669f3603281f6ecce33
HananIbrahimNouman <hanan.nouman@tuwaiq.edu.sa>
2022-11-15 23:56:18 +0300

    change the name and css

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -16,5 +16,5 @@ export * from "./NumberField/NumberField"
 export * from './Tooltip/Tooltip';
 export * from './LoadingButton/LoadingButton';
 export * from './Checkbox/Checkbox';
-export * from './Toggleswitch/Toggleswitch';
+export * from './Switch/Switch';
 export * from './Dialog/Dialog';
