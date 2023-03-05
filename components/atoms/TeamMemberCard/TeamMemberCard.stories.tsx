0733d6d8b830d445c8230efbdfa259984b1cc389
f1lander <filanderuclez@gmail.com>
2023-03-04 22:19:25 -0600

    fixes the teamMember card

--- components/atoms/TeamMemberCard/TeamMemberCard.stories.tsx
+++ components/atoms/TeamMemberCard/TeamMemberCard.stories.tsx
@@ -16,46 +16,146 @@ const Template: ComponentStory<typeof TeamMemberCard> = (args) => (
 
 const listMembers: EcosystemCardProps[] = [
   {
-    image: "https://obol.tech/assets/team/Edax.png",
     heading: "Collin Myers",
-    subheading: "CEO Founder",
+    subheading: "Project Lead, Founder",
     links: [
-      { link: "https://github.com/collin", network: "twitter" },
-      { link: "https://github.com/collin", network: "github" },
+      { link: "https://twitter.com/stakeeth", network: "twitter" },
+      { link: "https://github.com/collinjmyers", network: "github" },
+    ],
+
+    image: "https://obol.tech/assets/team/Collin.png",
+  },
+  {
+    heading: "Oisín Kyne",
+    subheading: "Head of Technology, Co-Founder",
+    links: [
+      { link: "https://twitter.com/oisinkyne", network: "twitter" },
+      { link: "https://github.com/oisinkyne", network: "github" },
+    ],
+    image: "https://obol.tech/assets/team/Oisin.jpg",
+  },
+  {
+    heading: "Aly Saleh",
+    image: "https://obol.tech/assets/team/Saleh.jpg",
+    subheading: "DevOps Lead",
+    links: [
+      { link: "https://twitter.com/eth2devops", network: "twitter" },
+      { link: "https://github.com/eth2devops", network: "github" },
     ],
   },
   {
+    heading: "Corver Roos",
+    image: "https://obol.tech/assets/team/Corver.png",
+    subheading: "Tech Lead",
+    links: [
+      { link: "https://twitter.com/corverdev", network: "twitter" },
+      { link: "https://github.com/corverroos", network: "github" },
+    ],
+  },
+  {
+    heading: "Chris Battenfield",
+    image: "https://obol.tech/assets/team/Chris.png",
+    subheading: "Product Lead",
+    links: [
+      { link: "https://twitter.com/battenfield", network: "twitter" },
+      { link: "https://github.com/battenfield", network: "github" },
+    ],
+  },
+  {
+    heading: "Dhruv Bodani",
+    image: "https://obol.tech/assets/team/Dhruv.png",
+    subheading: "Software Engineer",
+    links: [
+      { link: "https://twitter.com/dhruvbodani", network: "twitter" },
+      { link: "https://github.com/dB2510", network: "github" },
+    ],
+  },
+  {
+    heading: "Abishek Kumar",
+    image: "https://obol.tech/assets/team/Abhishek.png",
+    subheading: "Software Engineer",
+    links: [
+      { link: "https://twitter.com/xenowits", network: "twitter" },
+      { link: "https://github.com/xenowits", network: "github" },
+    ],
+  },
+  {
+    heading: "Edax Uclés",
     image: "https://obol.tech/assets/team/Edax.png",
-    heading: "Collin Myers",
+    subheading: "Full Stack / UI Engineer",
+    links: [
+      { link: "https://twitter.com/f1miyamoto", network: "twitter" },
+      { link: "https://github.com/f1lander", network: "github" },
+    ],
+  },
+  {
+    heading: "Richard Malone",
+    image: "https://obol.tech/assets/team/RichardMalone.png",
+    subheading: "Head of Business",
+    links: [
+      { link: "https://twitter.com/crypto_malone", network: "twitter" },
+      { link: "https://github.com/richardtmalone", network: "github" },
+    ],
+  },
+  {
+    heading: "Thomas Heremans",
+    image: "https://obol.tech/assets/team/Thomas.png",
+    subheading: "Technical Project Manager",
+    links: [
+      { link: "https://twitter.com/cryp_thomas_", network: "twitter" },
+      { link: "https://github.com/thomasheremans", network: "github" },
+    ],
+  },
+  {
+    heading: "Hanan Nouman",
+    image: "https://obol.tech/assets/team/Hanan.png",
+    subheading: "Frontend Engineer",
+    links: [
+      { link: "https://twitter.com/hanannouman", network: "twitter" },
+      { link: "https://github.com/HananINouman", network: "github" },
+    ],
+  },
+  {
+    heading: "Brett Li",
+    image: "https://obol.tech/assets/team/Brett.png",
+    subheading: "Growth Lead",
+    links: [
+      { link: "https://twitter.com/ComposeBrett", network: "twitter" },
+      { link: "https://github.com/Composeus", network: "github" },
+    ],
+  },
+  {
+    heading: "Jenia Barkanova",
+    image: "https://obol.tech/assets/team/Jenia.png",
     subheading: "Marketing Strategy Advisor",
+    links: [{ link: "https://twitter.com/JeniaBark", network: "twitter" }],
+  },
+  {
+    heading: "Francis Hackett",
+    image: "https://obol.tech/assets/team/Francis.png",
+    subheading: "Legal Counsel",
+    links: [{ link: "https://twitter.com/lexstake1", network: "twitter" }],
+  },
+  {
+    heading: "Luke Hackett",
+    image: "https://obol.tech/assets/team/Luke.png",
+    subheading: "Software Engineer",
     links: [
-      { link: "https://github.com/collin", network: "twitter" },
-      { link: "https://github.com/collin", network: "github" },
+      { link: "https://twitter.com/Luke_ObolLabs", network: "twitter" },
+      { link: "https://github.com/LukeHackett12", network: "github" },
     ],
   },
   {
-    image: "https://obol.tech/assets/team/Edax.png",
-    heading: "Collin Myers",
-    subheading: "Technical Project Manager PM founder",
-    links: [
-      { link: "https://github.com/collin", network: "twitter" },
-      { link: "https://github.com/collin", network: "github" },
-    ],
-  },
-  {
-    image: "https://obol.tech/assets/team/Edax.png",
-    heading: "Collin Myers",
-    subheading: "Ceo Founder",
-    links: [
-      { link: "https://github.com/collin", network: "twitter" },
-      { link: "https://github.com/collin", network: "github" },
-    ],
+    heading: "Nanni Sackmann",
+    image: "https://obol.tech/assets/team/Nanni.png",
+    subheading: "Head of People",
+    links: [{ link: "https://twitter.com/nannisack", network: "twitter" }],
   },
 ];
 
 const TemplateArray: ComponentStory<typeof TeamMemberCard> = (args) => {
   return (
-    <Box css={{ display: "flex", gap: "$md" }}>
+    <Box css={{ display: "flex", "@sm": { fd: "column" }, gap: "$md" }}>
       {listMembers.map((member, index) => (
         <TeamMemberCard key={`o:card-${index}:`} {...member} />
       ))}
