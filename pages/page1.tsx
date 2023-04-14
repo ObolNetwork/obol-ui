c179164e34a4b52e52002a386fabc5f4e3ffc2c0
f1lander <filanderuclez@gmail.com>
2023-04-14 15:10:32 -0600

    test new next link enhancement on nextjs13

---
+++ pages/page1.tsx
@@ -0,0 +1,12 @@
+import { Link } from "../components/atoms/Link/Link";
+
+export default function Page1() {
+    return (
+        <div>
+        <h1>Page 1</h1>
+        <Link href="/page2">
+            Go to page 2
+        </Link>
+        </div>
+    );
+    }; 
---
+++ pages/page2.tsx
@@ -0,0 +1,10 @@
+import { Link } from "../components/atoms/Link/Link";
+
+export default function Page2() {
+  return (
+    <div>
+      <h1>Page 2</h1>
+      <Link href="/page1">Go to page 1</Link>
+    </div>
+  );
+}
