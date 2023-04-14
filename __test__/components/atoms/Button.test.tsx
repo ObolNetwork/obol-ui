9112dbe2c3f7a4884110d8f535cc40026ea951d0
f1lander <filanderuclez@gmail.com>
2023-04-14 15:55:30 -0600

    updates button test

--- __test__/components/atoms/Button.test.tsx
+++ __test__/components/atoms/Button.test.tsx
@@ -1,21 +1,41 @@
 import { Button } from "../../../components/atoms/Button/Button";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom';
 
 describe("Button Component", () => {
-  describe("On Click Listener", () => {
-    const user = userEvent.setup();
-    const mockProps = {
-      onClick: jest.fn(),
-    };
+  const mockProps = {
+    onClick: jest.fn(),
+  };
 
+  describe("Rendering", () => {
+    test("Renders with text content", () => {
+      render(<Button {...mockProps}>test</Button>);
+      expect(screen.getByText("test")).toBeInTheDocument();
+    });
+
+    // Add more rendering tests here if needed
+  });
+
+  describe("User Interactions", () => {
     test("Calls onClick function passed in", async () => {
       render(<Button {...mockProps}>test</Button>);
 
       const testButton = screen.getByText("test");
-      await user.click(testButton);
+      await userEvent.click(testButton);
 
       expect(mockProps.onClick).toHaveBeenCalledTimes(1);
     });
+
+    // Add more interaction tests here if needed
+  });
+
+  describe("Accessibility", () => {
+    test("Has accessible role", () => {
+      render(<Button {...mockProps}>test</Button>);
+      expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+
+    // Add more accessibility tests here if needed
   });
 });
